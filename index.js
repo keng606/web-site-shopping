0cca2aabe8ba9c442a6714d87dce69786e7d57e7
Manote Khamthon <manotekhamthon@macbook-air-manote.local>
2025-04-05 01:41:09 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.slideshow').slick( );
+    $('.slideshow .slick-prev ').html('<i class="fa-solid fa-angle-left"></i>');
+    $('.slideshow .slick-next ').html('<i class="fa-solid fa-chevron-right"></i>');
+});
